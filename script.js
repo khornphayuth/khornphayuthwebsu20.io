d16bb0b8f11fae4033123c9367c7df8cf9d31409
khornphayuth <phayuthkhorn@gmail.com>
2025-03-11 03:05:49 -0900

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function submitForm(event) {
     event.preventDefault(); // Prevent actual form submission
   
     // Get input values
-    document.getElementById("displayName").innerText =
-      document.getElementById("name").value;
-    document.getElementById("displayEmail").innerText =
-      document.getElementById("email").value;
+    document.getElementById("displayName").innerText = document.getElementById("name").value;
+    document.getElementById("displayEmail").innerText = document.getElementById("email").value;
   
     // Format salary value
     const salary = parseFloat(document.getElementById("salary").value);
@@ -15,18 +13,18 @@ function submitForm(event) {
     }).format(salary);
     document.getElementById("displaySalary").innerText = formattedSalary;
   
-    document.getElementById("displayHireDate").innerText =
-      document.getElementById("hireDate").value;
+    document.getElementById("displayHireDate").innerText = document.getElementById("hireDate").value;
   
     // Show success alert
     document.getElementById("successAlert").classList.remove("d-none");
-  
+
+    alert ("1.EmpName : " + document.getElementById("name").value + "\n"+ 
+        "2.Email : " + document.getElementById("email").value + "\n"+
+        "3.Salary : " + `$${parseFloat(document.getElementById("salary").value).toLocaleString(undefined, {minimumFractionDigits: 2})}` + "\n"+
+        "4.HireDate " + document.getElementById("hireDate").value + "\n"
+      )
     // Reset form fields
     document.getElementById("myForm").reset();
-    alert ("1.EmpName : " + document.getElementById('name').value + "\n"+ 
-        "2.Email : " + document.getElementById('email').value + "\n"+
-        "3.Salary : " + `$${parseFloat(document.getElementById('salary').value).toLocaleString(undefined, {minimumFractionDigits: 2})}` + "\n"+
-        "4.HireDate " + document.getElementById('hireDate').value + "\n")
 }
   
   function hideAlert() {
